9c6d3e0c3783a85f802b860a58b5f119e9879140
APprod <APprod@users.noreply.github.com>
2026-07-13 21:31:29 +0300

    build: updated build scripts now Release and Debug have separate folders, switching don't cause complete rebuild of raylib

--- build.bat
+++ build.bat
@@ -1,27 +1,28 @@
 @echo off
 
 
-set BUILD_TYPE=Debug
-set STATIC_LINKING=ON
+set "BUILD_TYPE=Debug"
+set "STATIC_LINKING=ON"
+set "BUILD_DIR=_build\Debug"
 
 :parse
 if "%1"=="" goto :endparse
-if "%1"=="release" set BUILD_TYPE=Release & shift & goto :parse
-if "%1"=="debug" set BUILD_TYPE=Debug & shift & goto :parse
-if "%1"=="static" set STATIC_LINKING=ON & shift & goto :parse
-if "%1"=="dynamic" set STATIC_LINKING=OFF & shift & goto :parse
+if "%1"=="release" set "BUILD_TYPE=Release" & set "BUILD_DIR=_build\Release" & shift & goto :parse
+if "%1"=="debug" set "BUILD_TYPE=Debug" & set "BUILD_DIR=_build\Debug" & shift & goto :parse
+if "%1"=="static" set "STATIC_LINKING=ON" & shift & goto :parse
+if "%1"=="dynamic" set "STATIC_LINKING=OFF" & shift & goto :parse
 :endparse
 
 echo on
 
 echo Building %BUILD_TYPE% configuration...
 
-cmake -S . -B_build -G "MinGW Makefiles" -DIS_STATIC=%STATIC_LINKING%  -DCMAKE_BUILD_TYPE=%BUILD_TYPE% 
+cmake -S . -B%BUILD_DIR% -G "MinGW Makefiles" -DIS_STATIC=%STATIC_LINKING%  -DCMAKE_BUILD_TYPE=%BUILD_TYPE% 
 if %errorlevel% neq 0 exit /b 
 
-cmake --build _build --config %BUILD_TYPE% --parallel
+cmake --build %BUILD_DIR% --config %BUILD_TYPE% --parallel
 if %errorlevel% neq 0 exit /b 
 
-xcopy "assets" "_build/assets/" /s /e /h /I /Y
+xcopy "assets" "%BUILD_DIR%/assets/" /s /e /h /I /Y
 if %errorlevel% neq 0 exit /b 
 
--- run.bat
+++ run.bat
@@ -1,7 +1,12 @@
-echo off
+@echo off
 
+set BUILD_DIR=_build\Debug
 
+if "%2"=="release" set BUILD_DIR=_build\Release
+if "%2"=="debug"   set BUILD_DIR=_build\Debug
+if "%1"=="release" set BUILD_DIR=_build\Release
+if "%1"=="debug"   set BUILD_DIR=_build\Debug
 
 call ./build.bat %*
-if %errorlevel% neq 0 exit /b 
-_build\app.exe
+if %errorlevel% neq 0 exit /b
+%BUILD_DIR%\app.exe

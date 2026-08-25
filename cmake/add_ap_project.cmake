e87296da3261b25019938ef5d80a412e875b2978
APprod <APprod@users.noreply.github.com>
2026-08-25 17:55:16 +0300

    build: fixed add_ap_project functon to properly build web build

--- cmake/add_ap_project.cmake
+++ cmake/add_ap_project.cmake
@@ -3,6 +3,7 @@
 # Usage: add_ap_project(mygame)
 # Requires ApEngine targets (call after add_subdirectory of the engine).
 
+set(APENGINE_WEB_DIR "${APENGINE_CMAKE_DIR}/../web" CACHE PATH "ApEngine web support files")
 function(add_ap_project target)
     FILE(GLOB_RECURSE MyCPPSources ${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/include/*.hpp)
     add_executable(${target} ${MyCPPSources})
@@ -16,7 +17,7 @@ function(add_ap_project target)
         set(ASSETS_DIR "${CMAKE_CURRENT_BINARY_DIR}/assets")
     endif()
 
-    # Engine assets form the base layer, app assets overwrite them.
+    # Engine assets form the base layer, app assets adds on top of them.
     set(COPY_ARGS "")
     if(EXISTS "${APENGINE_ASSETS_DIR}")
         list(APPEND COPY_ARGS COMMAND ${CMAKE_COMMAND} -E copy_directory "${APENGINE_ASSETS_DIR}" "${ASSETS_DIR}")
@@ -33,4 +34,26 @@ function(add_ap_project target)
         )
         add_dependencies(${target} copy-assets-${target})
     endif()
+    
+    # Web Configurations
+    if (EMSCRIPTEN)
+        # Tell Emscripten to build.
+        set_target_properties(${target} PROPERTIES SUFFIX ".html")
+        target_link_options(${target} 
+        PRIVATE -sUSE_GLFW=3 
+        PRIVATE --preload-file assets
+        PRIVATE -sGL_ENABLE_GET_PROC_ADDRESS=1
+        PRIVATE -sWASM=1
+        PRIVATE -sMAX_WEBGL_VERSION=2
+        PRIVATE -sALLOW_MEMORY_GROWTH=1
+        PRIVATE --shell-file ${APENGINE_WEB_DIR}/minshell.html
+        )
+    endif()
+    # Checks if OSX and links appropriate frameworks (Only required on MacOS)
+    if (APPLE)
+        target_link_libraries(${target} "-framework IOKit")
+        target_link_libraries(${target} "-framework Cocoa")
+        target_link_libraries(${target} "-framework OpenGL")
+    endif()
+
 endfunction()
